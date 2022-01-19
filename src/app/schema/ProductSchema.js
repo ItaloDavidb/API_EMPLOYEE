41c9eff14a7f3ad48da31f669bb087d8a7391831
matheusmoraes2 <matheusmoraes85@hotmail.com>
2022-01-18 21:56:50 -0300

    feat: create product schema

---
+++ src/app/schema/ProductSchema.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const ProductSchema = mongoose.Schema({
+  name: String,
+  category: String,
+  price: Number,
+  employee_id:{
+      type:String,
+      ref:"Employee"
+    },
+})
+
+const Product = mongoose.model('Product', ProductSchema);
+module.exports = Product;
