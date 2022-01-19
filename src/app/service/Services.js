2c671eff1e143d6d688b6833177039e552dd193f
Rafael Tomás <rafaeltomassouza@gmail.com>
2022-01-18 23:03:57 -0300

    feat: add CR

--- src/app/service/Services.js
+++ src/app/service/Services.js
@@ -1,14 +1,19 @@
-const database = require('somewhere');
+const database = require('../repository');
+
 class Services {
   constructor(nameSchema) {
     this.nameSchema = nameSchema;
   }
 
   async createRecord(data) {
-    return database[this.nameSchema].create(data);
-
+    return await database[this.nameSchema].create(data);
   }
 
+  async catchAllRecord() {
+    return await database[this.nameSchema].find({});
+  }
+
+  
 }
 
 
