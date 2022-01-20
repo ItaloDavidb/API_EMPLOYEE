afdcf51d123c13ed6e89cf34cbe25ecc0268013f
ItaloDavidb <italodavid2@hotmail.com>
2022-01-20 19:15:49 -0300

    feat:validation to route put

--- src/app/validation/employee/put.js
+++ src/app/validation/employee/put.js
@@ -3,10 +3,10 @@ const Joi = require('joi');
 module.exports = async (req,res,next) =>{
   try {
     const employeeschema = Joi.object({
-      name: Joi.string().required().trim(),
-      cpf: Joi.string().pattern(/^[0-9]+$/, 'numbers').length(11).required(),
-      office: Joi.string().required().valid('gerente','vendedor','caixa'),
-      birthday: Joi.date().max('now').greater('1-1-1900').required()
+      name: Joi.string().trim(),
+      cpf: Joi.string().pattern(/^[0-9]+$/, 'numbers').length(11),
+      office: Joi.string().valid('gerente','vendedor','caixa'),
+      birthday: Joi.date().max('now').greater('1-1-1900')
     });
     const {error} = await employeeschema.validate(req.body,{abortEarl:true});
     if(error) throw error;
