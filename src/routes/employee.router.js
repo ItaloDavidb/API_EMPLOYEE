2bc79315f60abdf91541e5882ca3be593f1bb484
ItaloDavidb <italodavid2@hotmail.com>
2022-01-19 21:53:52 -0300

    feat:validation route put

--- src/routes/employee.router.js
+++ src/routes/employee.router.js
@@ -4,7 +4,7 @@ const validation = require('../app/validation/employee/create');
 module.exports = (server, routes, prefix = '/api/v1/employee') => {
   routes.post('/',validation,EmployeeController.create);
   routes.get('/', EmployeeController.find);
-  routes.put('/:employee_id', EmployeeController.update);
+  routes.put('/:employee_id', validation,EmployeeController.update);
   routes.delete('/:employee_id', EmployeeController.delete);
   server.use(prefix, routes);
 
